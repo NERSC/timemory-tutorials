af8a00328e20d58be0a19f2492b5fb9251baaff1
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2020-06-24 19:36:43 -0700

    Python apps + custom dynamic instr app

---
+++ apps/python/basic.py
@@ -0,0 +1,20 @@
+#!@PYTHON_EXECUTABLE@
+
+import sys
+import json
+import timemory
+from timemory.util import auto_timer
+from timemory.profiler import profile
+
+def fib(n):
+    return n if n < 2 else (fib(n-1) + fib(n-2))
+
+@auto_timer()
+def run(n):
+    ''' Decorator and context manager for high-level pre-defined collection '''
+    fib(n)
+
+if __name__ == "__main__":
+    n = int(sys.argv[1] if len(sys.argv) > 1 else 10)
+    run(n)
+    timemory.finalize()
