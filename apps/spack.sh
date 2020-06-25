a8ee3d138b1ac87d204226f42bb2be92c62cd2d5
Jonathan R. Madsen <jrmadsen@lbl.gov>
2020-06-25 01:09:01 -0700

    cori-gpu module file update + spack update for kokkos

--- apps/spack.sh
+++ apps/spack.sh
@@ -6,7 +6,7 @@ OPTS="+openmp +profiling +serial +shared"
 # OPTS="${OPTS} +hwloc +memkind +numactl"
 
 # Comment out if desired
-# OPTS="${OPTS} +cuda +cuda_uvm +cuda_lambda +wrapper"
+OPTS="${OPTS} +cuda +cuda_uvm +cuda_lambda +wrapper cuda_arch=72"
 
 spack spec kokkos ${OPTS} $@
 spack install -v -j8 kokkos ${OPTS} $@
