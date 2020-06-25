1ac3a3d9420d436b0c1a2147155fbcb155368faf
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2020-06-25 03:22:52 -0700

    Minor updates to 09_timemory_run_binary_rewrite_regex

--- intro-short/09_timemory_run_binary_rewrite_regex/launch.sh
+++ intro-short/09_timemory_run_binary_rewrite_regex/launch.sh
@@ -11,11 +11,19 @@ cp ${BIN_DIR}/basic.trace ./09_no_impl
 cp ${BIN_DIR}/basic.trace ./09_no_impl_or_allreduce
 
 # exclude all functions matching pattern 'impl_*'
-timemory-run -E 'impl_*' -o ./09_no_impl.inst -- ./09_no_impl
+timemory-run \
+    -E 'impl_*' \
+    -o ./09_no_impl.inst \
+    -- ./09_no_impl
 # run instrumented exe
 ./09_no_impl.inst
 
 # exclude all functions which start with 'impl' or 'allreduce'
-timemory-run -E '^(impl|allreduce)' -o ./09_no_impl_or_allreduce.inst -- ./09_no_impl_or_allreduce
+# enable instrumenting C++ standard library functions
+timemory-run \
+    -E '^(impl|allreduce)' \
+    --stdlib \
+    -o ./09_no_impl_or_allreduce.inst \
+    -- ./09_no_impl_or_allreduce
 # run instrumented exe
 ./09_no_impl_or_allreduce.inst
