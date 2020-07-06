4d2c0a92f87e49c55c75ad3efd21cb59519f8ae8
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2020-07-06 14:06:43 -0700

    Update apps/build.sh

--- apps/build.sh
+++ apps/build.sh
@@ -1,15 +1,35 @@
 #!/bin/bash -e
 
+run-verbose()
+{
+    echo -e "\n##### Executing \"$@\"... #####\n"
+    eval $@
+}
+
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+
+: ${SOURCE_DIR:=$(bash -c "cd ${SCRIPT_DIR} && pwd")}
+: ${BINARY_DIR:=${SOURCE_DIR}/build.sh-apps}
+: ${TARGET_DIR:=$(bash -c "cd ${SCRIPT_DIR}/.. && pwd")}
+
+echo "Source directory: ${SOURCE_DIR}"
+echo "Binary directory: ${BINARY_DIR}"
+echo "Output directory: ${TARGET_DIR}"
+
+if [ "${1}" = "clean" ]; then
+    run-verbose rm -rf ${BINARY_DIR}
+    exit 0
+fi
+
 # create build directory
-mkdir -p build-apps
-cd build-apps
+run-verbose mkdir -p ${BINARY_DIR}
+run-verbose cd ${BINARY_DIR}
 
 # remove any previous installs
 # rm -rf $(find ../../bin | grep -v gitignore)
 # rm -rf $(find ../../lib | grep -v gitignore)
 
 # install to root folder
-cmake -DCMAKE_INSTALL_PREFIX=../.. $@ ..
-
-make -j8
-make -j8 install
+run-verbose cmake -DCMAKE_INSTALL_PREFIX=${TARGET_DIR} $@ ${SOURCE_DIR}
+run-verbose cmake --build ${BINARY_DIR} --target all -- -j8
+run-verbose cmake --build ${BINARY_DIR} --target install -- -j8
